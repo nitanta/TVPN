8375f5b8cbd6aa7767557c3f50ca105b9ea14aa8
Nitanta Adhikari <nitanta.adh007@gmail.com>
2023-02-26 17:24:26 +0545

    Initial Commit

---
+++ TVPN/TVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVPNApp.swift
+//  TVPN
+//
+//  Created by Nitanta Adhikari on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
